646c71318f54a1745c2d43f593eddae369403cbd
Peter Barron <pbarron@gilt.com>
2014-03-18 13:19:47 +0000

    Add support to write and read Option[Array[*]]
    Change-Id: I618b4f26bb580335d0f04f24458b26ce33e662cd

--- src/test/scala/com/gilt/pickling/avro/AvroPicklingOptionArrayOfPrimitivesTest.scala
+++ src/test/scala/com/gilt/pickling/avro/AvroPicklingOptionArrayOfPrimitivesTest.scala
@@ -9,8 +9,9 @@ import org.apache.avro.generic.GenericData
 import scala.pickling._
 import java.util.{List => JList}
 import scala.Some
-import com.gilt.pickling.avro.AvroPicklingOptionArrayOfPrimitivesTest.OptionArrayOfInts
+import com.gilt.pickling.avro.AvroPicklingOptionArrayOfPrimitivesTest._
 import scala.collection.JavaConversions._
+import java.nio.ByteBuffer
 
 object AvroPicklingOptionArrayOfPrimitivesTest {
 
@@ -66,6 +67,222 @@ class AvroPicklingOptionArrayOfPrimitivesTest extends FunSuite with Assertions w
     }
   }
 
+  // Array of Longs
+  test("Pickle a case class with an optional array of longs") {
+    forAll {
+      (obj: OptionArrayOfLongs) =>
+        val pckl = obj.pickle
+        assert(generateBytesFromAvro(obj.list.map(_.toList), "/avro/option-array/OptionArrayOfLongs.avsc") === pckl.value)
+    }
+  }
+
+  test("Unpickle a case class with an optional array of longs") {
+    forAll {
+      (obj: OptionArrayOfLongs) =>
+        val bytes = generateBytesFromAvro(obj.list.map(_.toList), "/avro/option-array/OptionArrayOfLongs.avsc")
+        val hydratedObj: OptionArrayOfLongs = bytes.unpickle[OptionArrayOfLongs]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  test("Round trip a case class with an optional array of longs") {
+    forAll {
+      (obj: OptionArrayOfLongs) =>
+        val pckl = obj.pickle
+        val hydratedObj: OptionArrayOfLongs = pckl.unpickle[OptionArrayOfLongs]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  // Array of Doubles
+  test("Pickle a case class with an optional array of Doubles") {
+    forAll {
+      (obj: OptionArrayOfDoubles) =>
+        val pckl = obj.pickle
+        assert(generateBytesFromAvro(obj.list.map(_.toList), "/avro/option-array/OptionArrayOfDoubles.avsc") === pckl.value)
+    }
+  }
+
+  test("Unpickle a case class with an optional array of Doubles") {
+    forAll {
+      (obj: OptionArrayOfDoubles) =>
+        val bytes = generateBytesFromAvro(obj.list.map(_.toList), "/avro/option-array/OptionArrayOfDoubles.avsc")
+        val hydratedObj: OptionArrayOfDoubles = bytes.unpickle[OptionArrayOfDoubles]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  test("Round trip a case class with an optional array of Doubles") {
+    forAll {
+      (obj: OptionArrayOfDoubles) =>
+        val pckl = obj.pickle
+        val hydratedObj: OptionArrayOfDoubles = pckl.unpickle[OptionArrayOfDoubles]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  // Array of Floats
+  test("Pickle a case class with an optional array of Floats") {
+    forAll {
+      (obj: OptionArrayOfFloats) =>
+        val pckl = obj.pickle
+        assert(generateBytesFromAvro(obj.list.map(_.toList), "/avro/option-array/OptionArrayOfFloats.avsc") === pckl.value)
+    }
+  }
+
+  test("Unpickle a case class with an optional array of Floats") {
+    forAll {
+      (obj: OptionArrayOfFloats) =>
+        val bytes = generateBytesFromAvro(obj.list.map(_.toList), "/avro/option-array/OptionArrayOfFloats.avsc")
+        val hydratedObj: OptionArrayOfFloats = bytes.unpickle[OptionArrayOfFloats]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  test("Round trip a case class with an optional array of Floats") {
+    forAll {
+      (obj: OptionArrayOfFloats) =>
+        val pckl = obj.pickle
+        val hydratedObj: OptionArrayOfFloats = pckl.unpickle[OptionArrayOfFloats]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  // Array of Booleans
+  test("Pickle a case class with an optional array of Booleans") {
+    forAll {
+      (obj: OptionArrayOfBooleans) =>
+        val pckl = obj.pickle
+        assert(generateBytesFromAvro(obj.list.map(_.toList), "/avro/option-array/OptionArrayOfBooleans.avsc") === pckl.value)
+    }
+  }
+
+  test("Unpickle a case class with an optional array of Booleans") {
+    forAll {
+      (obj: OptionArrayOfBooleans) =>
+        val bytes = generateBytesFromAvro(obj.list.map(_.toList), "/avro/option-array/OptionArrayOfBooleans.avsc")
+        val hydratedObj: OptionArrayOfBooleans = bytes.unpickle[OptionArrayOfBooleans]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  test("Round trip a case class with an optional array of Booleans") {
+    forAll {
+      (obj: OptionArrayOfBooleans) =>
+        val pckl = obj.pickle
+        val hydratedObj: OptionArrayOfBooleans = pckl.unpickle[OptionArrayOfBooleans]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  // Array of Strings
+  test("Pickle a case class with an optional array of Strings") {
+    forAll {
+      (obj: OptionArrayOfStrings) =>
+        val pckl = obj.pickle
+        assert(generateBytesFromAvro(obj.list.map(_.toList), "/avro/option-array/OptionArrayOfStrings.avsc") === pckl.value)
+    }
+  }
+
+  test("Unpickle a case class with an optional array of Strings") {
+    forAll {
+      (obj: OptionArrayOfStrings) =>
+        val bytes = generateBytesFromAvro(obj.list.map(_.toList), "/avro/option-array/OptionArrayOfStrings.avsc")
+        val hydratedObj: OptionArrayOfStrings = bytes.unpickle[OptionArrayOfStrings]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  test("Round trip a case class with an optional array of Strings") {
+    forAll {
+      (obj: OptionArrayOfStrings) =>
+        val pckl = obj.pickle
+        val hydratedObj: OptionArrayOfStrings = pckl.unpickle[OptionArrayOfStrings]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  // Array of Bytes
+  test("Pickle a case class with an optional array of Bytes") {
+    forAll {
+      (obj: OptionArrayOfBytes) =>
+        val pckl = obj.pickle
+        assert(generateBytesFromAvroWithByteBuffer(obj.list.map(ByteBuffer.wrap(_)), "/avro/option-array/OptionArrayOfBytes.avsc") === pckl.value)
+    }
+  }
+
+  test("Unpickle a case class with an optional array of Bytes") {
+    forAll {
+      (obj: OptionArrayOfBytes) =>
+        val bytes = generateBytesFromAvroWithByteBuffer(obj.list.map(ByteBuffer.wrap(_)), "/avro/option-array/OptionArrayOfBytes.avsc")
+        val hydratedObj: OptionArrayOfBytes = bytes.unpickle[OptionArrayOfBytes]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  test("Round trip a case class with an optional array of Bytes") {
+    forAll {
+      (obj: OptionArrayOfBytes) =>
+        val pckl = obj.pickle
+        val hydratedObj: OptionArrayOfBytes = pckl.unpickle[OptionArrayOfBytes]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  // Array of Shorts
+  test("Pickle a case class with an optional array of Shorts") {
+    forAll {
+      (obj: OptionArrayOfShorts) =>
+        val pckl = obj.pickle
+        assert(generateBytesFromAvro(obj.list.map(_.toList.map(_.toInt)), "/avro/option-array/OptionArrayOfShorts.avsc") === pckl.value)
+    }
+  }
+
+  test("Unpickle a case class with an optional array of Shorts") {
+    forAll {
+      (obj: OptionArrayOfShorts) =>
+        val bytes = generateBytesFromAvro(obj.list.map(_.toList.map(_.toInt)), "/avro/option-array/OptionArrayOfShorts.avsc")
+        val hydratedObj: OptionArrayOfShorts = bytes.unpickle[OptionArrayOfShorts]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  test("Round trip a case class with an optional array of Shorts") {
+    forAll {
+      (obj: OptionArrayOfShorts) =>
+        val pckl = obj.pickle
+        val hydratedObj: OptionArrayOfShorts = pckl.unpickle[OptionArrayOfShorts]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  // Array of Chars
+  test("Pickle a case class with an optional array of Chars") {
+    forAll {
+      (obj: OptionArrayOfChars) =>
+        val pckl = obj.pickle
+        assert(generateBytesFromAvro(obj.list.map(_.toList.map(_.toInt)), "/avro/option-array/OptionArrayOfChars.avsc") === pckl.value)
+    }
+  }
+
+  test("Unpickle a case class with an optional array of Chars") {
+    forAll {
+      (obj: OptionArrayOfChars) =>
+        val bytes = generateBytesFromAvro(obj.list.map(_.toList.map(_.toInt)), "/avro/option-array/OptionArrayOfChars.avsc")
+        val hydratedObj: OptionArrayOfChars = bytes.unpickle[OptionArrayOfChars]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
+  test("Round trip a case class with an optional array of Chars") {
+    forAll {
+      (obj: OptionArrayOfChars) =>
+        val pckl = obj.pickle
+        val hydratedObj: OptionArrayOfChars = pckl.unpickle[OptionArrayOfChars]
+        assert(obj.list.map(_.toList) === hydratedObj.list.map(_.toList))
+    }
+  }
+
   private def generateBytesFromAvro(value: Option[JList[_]], schemaFileLocation: String): Array[Byte] = {
     val schema: Schema = retrieveAvroSchemaFromFile(schemaFileLocation)
     val record = new GenericData.Record(schema)
@@ -73,12 +290,17 @@ class AvroPicklingOptionArrayOfPrimitivesTest extends FunSuite with Assertions w
       case Some(x) => record.put("list", x)
       case _ => record.put("list", null)
     }
-    try {
-      convertToBytes(schema, record)
-    } catch {
-      case e: Throwable =>
-        println(e)
-        throw e
-    }
+    convertToBytes(schema, record)
   }
+
+  private def generateBytesFromAvroWithByteBuffer(value: Option[ByteBuffer], schemaFileLocation: String): Array[Byte] = {
+    val schema: Schema = retrieveAvroSchemaFromFile(schemaFileLocation)
+    val record = new GenericData.Record(schema)
+    value match {
+      case Some(x) => record.put("list", x)
+      case _ => record.put("list", null)
+    }
+    convertToBytes(schema, record)
+  }
+
 }
